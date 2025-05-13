486ebb714b0e9b02083d15ccadb06e1b0057ef00
vivianalicia <vivian.alicia.va@gmail.com>
2025-05-13 11:49:43 +0700

    fixing style collection

--- js/song-detail.js
+++ js/song-detail.js
@@ -1,1 +1,182 @@
-document.
+document.addEventListener("DOMContentLoaded", function(){
+    const songs = [
+        {
+            id: '1',
+            title: 'FE!N',
+            artist: 'Travis Scott',
+            description: 'FE!N” is a bold, immersive album where Travis Scott blends futuristic trap with introspective themes and experimental sounds.',
+            cover: '../assets/Collection/Pop/travis scott - UTOPIA.jpeg',
+            file: '../assets/Songs/Travis Scott Ft. Playboi Carti & Sheck Wes – FEIN',
+            lyric: `[Intro: Travis Scott & Sheck Wes]
+                    Just come outside for the night (Yeah)
+                    Take your time, get your light (Yeah)
+                    Johnny Dang, yeah, yeah
+                    I been out geekin' (Bitch)
+
+                    [Chorus: Travis Scott]
+                    FE!N, FE!N, FE!N, FE!N, FE!N (Yeah)
+                    FE!N, FE!N, FE!N, FE!N, FE!N (Yeah)
+                    FE!N, FE!N, FE!N, FE!N, FE!N
+                    FE!N, FE!N (Yeah), FE!N, FE!N, FE!N
+
+                    [Verse 1: Travis Scott & Sheck Wes]
+                    The career's more at stake when you in your prime (At stake)
+                    Fuck that paper, baby, my face on the dotted line (Dot, yeah)
+                    I been flyin' out of town for some peace of mind (Yeah, yeah, bitch)
+                    It's like always they just want a piece of mine (Ah)
+                    I been focused on the future, never on right now (Ah)
+                    What I'm sippin' not kombucha, either pink or brown (It's lit)
+                    I'm the one that introduced you to the you right now (Mm, let's go)
+                    Oh my God, that bitch bitin' (That bitch bitin')
+                    Well, alright (Alright), tryna vibe (I'm tryna vibe this)
+                    In the night, come alive
+                    Ain't asleep, ain't a—, ain't a—, ain't-ain't
+
+                    [Chorus: Travis Scott]
+                    FE!N, FE!N, FE!N, FE!N, FE!N
+                    FE!N, FE!N, FE!N, FE!N, FE!N
+                    FE!N, FE!N, FE!N, FE!N, FE!N
+                    FE!N, FE!N, FE!N, FE!N
+                    FE!N, FE!N, FE!N, FE!N, FE!N
+                    See Travis Scott Live
+                    Get tickets as low as $30
+                    You might also like
+                    DELRESTO (ECHOES)
+                    Beyoncé & Travis Scott
+                    bad idea right?
+                    Olivia Rodrigo
+                    Used To Be Young
+                    Miley Cyrus
+                    [Bridge: Playboi Carti]
+                    Schyeah, woah, what?
+                    What?
+                    (Homixide, Homixide, Homixide, Homixide)
+                    What? (Yeah)
+                    Woah, woah (Yeah, yeah)
+                    (Homixide, Homixide, Homixide, Homixide)
+                    Hit, yeah, hold up (Yeah)
+
+                    [Verse 2: Playboi Carti]
+                    Yeah, I just been poppin' my shit and gettin' it live, hold up (Shit)
+                    Yeah, you try to come wrong 'bout this shit, we poppin' your tires, hold up (Shit)
+                    Uh, hundred-round (Woah), feelin' like I'm on ten
+                    Playin' both sides with these hoes (Hold up), shawty, I'm fuckin' your friend (Hold up)
+                    I've been goin' crazy, shawty, I've been in the deep end
+                    She not innocent, uh, she just tryna go
+
+                    [Chorus: Travis Scott & Playboi Carti]
+                    FE!N (Talkin' 'bout), FE!N, FE!N (Schyeah), FE!N, FE!N (Schyeah, oh, oh, what? Schyeah)
+                    FE!N, FE!N (Schyeah), FE!N, FE!N, FE!N (Oh, oh)
+                    FE!N, FE!N (Talkin' 'bout), FE!N, FE!N, FE!N, FE!N (Talkin' 'bout, let's go)
+
+                    [Verse 3: Playboi Carti & Travis Scott]
+                    I just been icin' my hoes, I just been drippin' my hoes (Drippin' my hoes)
+                    This is a whole 'nother level, shawty (Oh), I got these hoes on they toes (Hoes on they toes)
+                    I put the bitch on the road, she tryna fuck on the O, hold up, hold up
+                    I got this ho with me, she tryna show me somethin', hold up, hold up (Oh)
+                    I got flows for days, these niggas ain't on nothin', hold up, yeah (Oh)
+                    Me and my boy locked in, you know we on one, hold up, uh (Slatt, slatt)
+                    We in the spot goin' crazy until the sun up
+                    You worried about that ho, that ho done chose up (Slatt, bitch-ass)
+                    Uh, pistols all in the kitchen, can't give the zip code up, hold up, yeah, slatt (Wow)
+                    FE!N, FE!N, FE!N (Huh? Huh? Huh? Huh? Yeah)
+                    Why the fuck these niggas actin' like they know us?
+                    00CACTUS, yeah, we towed up (Skrrt, skrrt), uh, yeah
+                    Switch out the bag, these niggas get rolled up, hold up (It's lit), slatt
+                    Everything hit, hold up, everything Homixide, Homixide (Homixide, Homixide, Homixide, Homixide)
+                    [Outro: Travis Scott & Playboi Carti]
+                    FE!N, FE!N, FE!N, FE!N, FE!N, FE!N (Homixide, Homixide, Homixide, Homixide, Homixide, Homixide, Homixide)`
+            
+            
+        },
+        {
+            id:'2',
+            title: 'WILDFLOWER',
+            artist: 'Billie Eilish',
+            description: 'Wildflower" is a folk-pop ballad by Billie Eilish about the guilt of falling for a friend’s ex. With soft acoustic tones and raw emotion, it showcases her intimate storytelling.',
+            cover: '../assets/Collection/Pop/billiecover.jpeg',
+            file: '../assets/Songs/',
+            lyric: `[Verse 1]
+                    Things fall apart
+                    And time breaks your heart
+                    I wasn't there, but I know
+                    She was your girl
+                    You showed her the world
+                    But fell out of love and you both let go
+
+                    [Pre-Chorus]
+                    She was cryin’ on my shoulder
+                    All I could do was hold her
+                    Only made us closer until July
+                    Now, I know that you love me
+                    You don't need to remind me
+                    I should put it all behind me, shouldn't I?
+
+                    [Chorus]
+                    But I see her in the back of my mind all the time
+                    Like a fever, like I’m burning alive, like a sign
+                    Did I cross the line?
+                    (Mm) Hmm
+
+                    [Verse 2]
+                    Well, good things don't last (Good things don't last)
+                    And life moves so fast (Life moves so fast)
+                    I'd never ask who was better (I'd never ask who was better)
+                    'Cause she couldn't be (Couldn't)
+                    More different from me (Different)
+                    Happy and free in leather (Happy)
+                    See Billie Eilish Live
+                    Get tickets as low as $207
+                    You might also like
+                    Guess featuring billie eilish
+                    Charli xcx & Billie Eilish
+                    Darling, I
+                    Tyler, The Creator
+                    Juno
+                    Sabrina Carpenter
+                    [Pre-Chorus]
+                    And I know that you love me (You love me)
+                    You don’t need to remind me (Remind me)
+                    Wanna put it all behind me, but baby
+
+                    [Chorus]
+                    I see her in the back of my mind (Back of my mind) all the time (All the time)
+                    Feels like a fever (Like a fever), like I’m burning alive (Burning alive), like a sign
+                    Did I cross the line?
+
+                    [Bridge]
+                    You say no one knows you so well (Oh)
+                    But every time you touch me, I just wonder how she felt
+                    Valentine's Day, cryin’ in the hotel
+                    I know you didn't mean to hurt me, so I kept it to myself
+
+                    [Chorus]
+                    And I wonder
+                    Do you see her in the back of your mind in my eyes?
+
+                    [Outro]
+                    You say no one knows you so well
+                    But every time you touch me, I just wonder how she felt
+                    Valentine's Day, cryin' in the hotel
+                    I know you didn’t mean to hurt me, so I kept it to myself`,
+        }
+    ];
+    const params = new URLSearchParams(window.location.search);
+    const songId = params.get('id');
+    const song = songs.find(s=> s.id == songId);
+
+    if (song){
+        document.querySelectorAll('.song-name-value').forEach(el => {
+            el.textContent = song.title;
+        });
+        document.querySelectorAll('.song-artist-value').forEach(el => {
+            el.textContent = song.artist;
+        });
+        document.querySelector('.song-desc-value').textContent = song.description;
+        document.querySelectorAll('.song-cover').forEach(el => {
+            el.src = song.cover;
+        });
+        document.querySelector('.lyrics').textContent = song.lyric;
+
+    }
+})
